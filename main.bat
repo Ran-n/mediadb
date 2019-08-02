5a39a874361dab9839c885ca61ba7fd5fd951374
Ran# <31899984+Ran-n@users.noreply.github.com>
2019-08-02 17:12:35 +0200

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,4 +1,4 @@
-#+ Autor:	Ran#
-#+ Creado:	02/08/2019 17:08:21
-#+ Editado:	02/08/2019 17:08:21
+::+ Autor:	Ran#
+::+ Creado:	02/08/2019 17:08:21
+::+ Editado:	02/08/2019 17:08:21
 py src/mediadb.py
